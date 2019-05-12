e4a480a5c10f1c6bed1fcb31f027e81ca45253b0
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-12 09:18:23 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,37 @@
-# require libraries/modules here
+
+require 'nokogiri'
+require "pry"
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')     #open the file
+  kickstarter = Nokogiri::HTML(html)                #directory where data is kept for tests
+  #binding.pry
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+ 
+  # return the projects hash
+  projects
 end
+
+
+#create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+# image link: project.css("div.project-thumbnail a img").attribute("src").value  
+#an img tag in html is considered to have a source attribute ..You can use the .attribute method on a Nokogiri element to grab the value of that attribute.
